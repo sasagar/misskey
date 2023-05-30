19dac2d2465bbf0fbef95171ebfc1c6a6f0ba4d2
syuilo <Syuilotan@yahoo.co.jp>
2023-05-30 11:32:29 +0900

    vue-plyrやめるなど
    - 問題点が多い
    - バンドルサイズ削減

--- packages/frontend/src/components/MkMediaVideo.vue
+++ packages/frontend/src/components/MkMediaVideo.vue
@@ -29,7 +29,6 @@
 <script lang="ts" setup>
 import { ref } from 'vue';
 import * as misskey from 'misskey-js';
-import VuePlyr from 'vue-plyr';
 import bytes from '@/filters/bytes';
 import { defaultStore } from '@/store';
 import { i18n } from '@/i18n';
